b79e50c1247742f26dd8bf1990374fb2366b8592
student <student@mail.el-ts.ru>
2017-12-07 11:24:31 +0300

    Initial Commit

---
+++ GermesProject - light/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GermesProject - light
+//
+//  Created by student on 07.12.2017.
+//  Copyright © 2017 Ivan Bobrov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
